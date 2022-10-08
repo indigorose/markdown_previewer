d2e7482d9f5c00dea8222e41b0ea41f0846ad314
indigorose <indigorose049@gmail.com>
2022-10-08 08:05:21 +0100

    Initial notes and planning

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <textarea name="editor" id="editor" cols="30" rows="10"></textarea>
-      <div id="preview"></div>
+      <h1>Markdown Previewer</h1>
+      <div className="container--editor">
+        <div className="editor-header">
+          <h2>(icon)Editor</h2>
+        </div>
+        <textarea name="editor" id="editor" cols="30" rows="10"></textarea>
+      </div>
+      <div className="container--previewer">
+        <div className="previewer-header">
+          <h2>(icon)Previwer</h2>
+        </div>
+        <div id="preview" className="previewer-body">
+          This should show the results of the textarea
+        </div>
+      </div>
+      <p>by Karen Robertson</p>
     </div>
   );
 }
