b70beb928f78bdefd2a6cb4c16bc9c806a8d5217
indigorose <indigorose049@gmail.com>
2023-06-12 21:48:11 +0100

    Update styling

--- src/App.js
+++ src/App.js
@@ -4,82 +4,82 @@ import ReactMarkdown from 'react-markdown';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputValue: placeholder,
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(e) {
-    this.setState({
-      inputValue: e.target.value,
-    });
-  }
-  render() {
-    return (
-      // This layout is the parent, the textarea is the first child and previewer is the second and nest in the parent app.
-      <div className="App">
-        <header>
-          <h1>Markdown Previewer</h1>
-        </header>
-        <div className="container">
-          <div className="container--editor">
-            <div className="editor-header">
-              <h2>
-                <FontAwesomeIcon icon={faArrowRight} />
-              </h2>
-              <h2>Editor</h2>
-            </div>
-            <TextInput
-              input={this.state.inputValue}
-              handleChange={this.handleChange}
-            />
-          </div>
-          <div className="container--previewer">
-            <div className="previewer-header">
-              <h2>
-                <FontAwesomeIcon icon={faArrowRight} />
-              </h2>
-              <h2>Previewer</h2>
-            </div>
-            <div id="preview" className="previewer-body">
-              <MarkDownText
-                classname="body-text"
-                input={this.state.inputValue}
-              />
-            </div>
-          </div>
-          <div className="footer">
-            <p>by Karen Robertson</p>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			inputValue: placeholder,
+		};
+		this.handleChange = this.handleChange.bind(this);
+	}
+	handleChange(e) {
+		this.setState({
+			inputValue: e.target.value,
+		});
+	}
+	render() {
+		return (
+			// This layout is the parent, the textarea is the first child and previewer is the second and nest in the parent app.
+			<div className="App">
+				<header>
+					<h1>Markdown Previewer</h1>
+				</header>
+				<div className="container">
+					<div className="container--editor">
+						<div className="editor-header">
+							<h2>
+								<FontAwesomeIcon icon={faArrowRight} />
+							</h2>
+							<h2>Editor</h2>
+						</div>
+						<TextInput
+							input={this.state.inputValue}
+							handleChange={this.handleChange}
+						/>
+					</div>
+					<div className="container--previewer">
+						<div className="previewer-header">
+							<h2>
+								<FontAwesomeIcon icon={faArrowRight} />
+							</h2>
+							<h2>Previewer</h2>
+						</div>
+						<div id="preview" className="previewer-body">
+							<MarkDownText
+								classname="body-text"
+								input={this.state.inputValue}
+							/>
+						</div>
+					</div>
+					<div className="footer">
+						<p>by Karen Robertson</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 class TextInput extends Component {
-  render() {
-    const textInput = this.props.input;
-    const textChange = this.props.handleChange;
-    return (
-      <textarea
-        id="editor"
-        cols="90"
-        rows="30"
-        value={textInput}
-        onChange={textChange}
-      />
-    );
-  }
+	render() {
+		const textInput = this.props.input;
+		const textChange = this.props.handleChange;
+		return (
+			<textarea
+				id="editor"
+				cols="90"
+				rows="30"
+				value={textInput}
+				onChange={textChange}
+			/>
+		);
+	}
 }
 
 class MarkDownText extends Component {
-  render() {
-    const textMark = this.props.input;
-    return <ReactMarkdown>{textMark}</ReactMarkdown>;
-  }
+	render() {
+		const textMark = this.props.input;
+		return <ReactMarkdown>{textMark}</ReactMarkdown>;
+	}
 }
 
 const placeholder = `# Welcome to my React Markdown Previewer!
@@ -124,6 +124,6 @@ And here. | Okay. | I think we get it.
 1. Use just 1s if you want!
 1. And last but not least, let's not forget embedded images:
 
-![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+![Markdown Logo](https://upload.wikimedia.org/wikipedia/commons/4/48/Markdown-mark.svg)
 `;
 export default App;
